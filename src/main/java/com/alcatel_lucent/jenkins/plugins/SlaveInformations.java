64b986859fc5a191603f5951dbd3265c8258c159
Olivier Ferreira <olivier.ferreira@alcatel-lucent.com>
2013-07-18 16:12:10 +0200

    source

---
+++ src/main/java/com/alcatel_lucent/jenkins/plugins/SlaveInformations.java
@@ -0,0 +1,46 @@
+package com.alcatel_lucent.jenkins.plugins;
+
+import jenkins.model.Jenkins;
+import hudson.Extension;
+import hudson.Plugin;
+import hudson.model.ManagementLink;
+import hudson.model.RootAction;
+
+
+/**
+ * @author Olivier Ferreira <br />
+ * 	olivier.ferreira@alcatel-lucent.com 
+ */
+
+public class SlaveInformations extends Plugin {
+
+	
+	private String name;
+	@Extension
+	public static class MyLink extends ManagementLink implements RootAction{
+
+		@Override
+		public String getDisplayName() {
+			return "Slave Information";
+		}
+		
+		@Override
+		public String getIconFileName() {
+			return "/plugin/slave/icon.jpg";
+		}
+	
+		@Override
+		public String getUrlName() {
+
+			return Jenkins.getInstance().getRootUrl()+ "plugin/slave/";
+		}
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
